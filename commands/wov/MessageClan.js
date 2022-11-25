8b673005f75a17aa451232ea6f17c13579ede649
Lusky34 <caramel.adamy@gmail.com>
2022-11-25 14:40:48 +0100

    Uptade message clan visiblité message v2

--- commands/wov/MessageClan.js
+++ commands/wov/MessageClan.js
@@ -52,7 +52,7 @@ module.exports = {
           .set( 'Authorization', process.env.WOV_TOKEN)
           .set('Content-Type', 'application/json')
           .set('Accept', 'application/json')
-          .catch((err) => { message.channel.send(`Erreur dans la requête du pseudo le message ${i} n'a pas pu être envoyé \n nouvelle tentative en cours...`); console.log (i)});          
+          .catch((err) => { message.channel.send(`Erreur dans la requête du pseudo le message ${i} n'a pas pu être envoyé.\n\`Nouvelle tentative en cours...\``); console.log (i)});          
           
          try{ var pseudo = usernameb.text} catch (err) {usernameb}
 
