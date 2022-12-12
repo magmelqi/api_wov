12d8bb8335f3d6d46b85f08b507b3a93da4988a2
Lusky34 <caramel.adamy@gmail.com>
2022-12-12 19:27:45 +0100

    fixe emojis

--- commands/wov/emojiSend.js
+++ commands/wov/emojiSend.js
@@ -15,7 +15,7 @@ module.exports = {
         if (!args[0] || !args[0].match(/^(poo|alien|alien_ship|alien_surprise|angel_happy|angel_wings|anger|angry_farmer|angry_monster|bearok|bear_pink|bear_blue)$/)) return message.reply('merci d\'entrer un nom d\'émoji valide');
         if (!args[1] || !args[1].match(/^(petit|moyen|grand)$/)) return message.reply('merci d\'entrer une taille valide (`petit`/`moyen`/`grand`)');
 
-            const emojis = JSON.parse(readFileSync(`././information/emojis/${args[0]}.json`, 'utf-8')); console.log(emojis, message.author.username)
+            const emojis = JSON.parse(readFileSync(`././Information/emojis/${args[0]}.json`, 'utf-8')); console.log(emojis, message.author.username)
             var lien = emojis.url; var lien2 = lien.slice(0, -4)
 
         if (args[1] == 'petit') { message.channel.send(`${lien2}.png`)}
@@ -83,8 +83,8 @@ module.exports = {
         const evtChoices = interaction.options.getString('taille');
         const nameChoices = interaction.options.getString('a-b' ?? 'b-c');
         try {
-            var DOr = readdirSync(`././information/emojis/`, 'utf-8')} catch (err) {return interaction.reply({content: `Erreur dans la lecture des fichiers: ${err}`, ephemeral: true});}
-            const emojis = JSON.parse(readFileSync(`././information/emojis/${nameChoices}.json`, 'utf-8')); console.log(emojis, interaction.user.username)
+            var DOr = readdirSync(`././Information/emojis/`, 'utf-8')} catch (err) {return interaction.reply({content: `Erreur dans la lecture des fichiers: ${err}`, ephemeral: true});}
+            const emojis = JSON.parse(readFileSync(`././Information/emojis/${nameChoices}.json`, 'utf-8')); console.log(emojis, interaction.user.username)
             var lien = emojis.url; var lien2 = lien.slice(0, -4)
 
         if (evtChoices == 'taille1') {interaction.reply(`${lien2}.png`);}
