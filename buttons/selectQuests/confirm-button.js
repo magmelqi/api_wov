902fae5f4e7cc3e070d786ac4111f4ba9873245c
Lusky34 <caramel.adamy@gmail.com>
2023-01-06 13:41:58 +0100

    Update bouton

--- buttons/selectQuests/confirm-button.js
+++ buttons/selectQuests/confirm-button.js
@@ -1,5 +1,6 @@
 const {readFileSync, readdirSync, unlinkSync} = require ("fs");
-const { MessageEmbed } = require ('discord.js')
+const { MessageEmbed, Formatters, MessageAttachment } = require ('discord.js');
+const dayjs = require("dayjs");
 
 module.exports = {
     name: "confirm-button",
@@ -50,8 +51,66 @@ module.exports = {
         
                   try {
                       unlinkSync(`././Information/Quête-info/${ListeO}`, 'utf-8')}catch (err) {}}
-            
-            interaction.channel.send(`Lancement de la quête ${nomF}... \nR.I.P suite du code inexistante`); console.log(InfoA.Id)} else {interaction.channel.send(`Annulation du lancement de la quête réussie`)}
+
+                     var jour = `${dayjs().format("ddd")}`; var j = 0
+
+      while (jour != "Tue") { var j = j +1
+             var jour = `${dayjs().add(+j, 'day').format("ddd")}`;
+      };
+      var date = `${dayjs().add(+j, 'day').format("YYYY-MM-DD")}`
+      
+         const realativeTempsF = Formatters.time(dayjs(`${date}T20:00`).unix(), Formatters.TimestampStyles.RelativeTime)
+         const TempsF = Formatters.time(dayjs(`${date}T20:00`).unix(), Formatters.TimestampStyles.ShortDateTime); 
+
+         var heure = `${dayjs().format("HH")}`;
+              var h = 0
+              while (heure != 21) { var h = h +1
+                     var heure = heure -1 +2
+                     if ( heure == 24) {var heure = 0}
+              };var h = h-1
+              var Hmili = h*60*60*1000; 
+
+              var min = `${dayjs().format("mm")}`
+              var m = 0
+              while (min != 00) { var m = m +1;
+                     var min = min -1 + 2
+                     if ( min == 60) {var min = 0}
+              };var m = m -1
+              var mMili = m*60*1000;
+
+              var sec = `${dayjs().format("ss")}`
+              var s = 0
+              while (sec != 00) { var s = s +1;
+                     var sec = sec -1 + 2
+                     if ( sec == 60) {var sec = 0}
+              }; var s = 60 - s +1
+              var sMili = s*1000;
+              var jMili = j*24*60*60*1000
+
+              var Mili = jMili+ Hmili + mMili - sMili
+                    
+              interaction.channel.send(`Lancement de la quête ${nomF}...\n${TempsF} (${realativeTempsF})`); console.log(InfoA.Id)
+                    console.log(Mili)
+                    var  Mili = Mili - 600000
+                    if (Mili < 540000) {return interaction.channel.send('Erreur: veuillez lancer la commande avant 20h50')}
+                    else {var image = new MessageAttachment(InfoA.Image)
+              await new Promise(resolve => setTimeout(resolve, Mili))
+            interaction.channel.send({content:"Lancement de la quête dans `\`\`10 min\`\`\nPour l'annuler faite la commande reload", files:[image]})
+            await new Promise(resolve => setTimeout(resolve, 540000))
+            interaction.channel.send("Lancement de la quête dans `\`\`1 min\`\`\nPour l'annuler faite la commande reload")
+            await new Promise(resolve => setTimeout(resolve, 60000))
+            var  QuestsClaim = await superagent.post(`https://api.wolvesville.com/clans/${process.env.CLAN_ID}/quests/claim`)
+            .send({questId: InfoA.Id})
+            .set( 'Authorization', process.env.WOV_TOKEN)
+            .set('Content-Type', 'application/json')
+            .set('Accept', 'application/json')
+            .catch((err) => { if (err == "Error: Too Many Requests") {interaction.channel.send({content:`Erreur, veuillez reconfirmer la quête`})}
+            else {return interaction.channel.send({content:`Erreur: ${err}`})}}); 
+        interaction.channel.send(`La quête a été lancé !`)
+        const QuêteChannel =  client.channels.cache.get('1044258472121860126')//817290280242774036
+        QuêteChannel.send(`Quête lancée ! <:panda_youpi:724242729046900756>`)}
+        interaction.message.delete()
+        } else {interaction.channel.send(`Annulation du lancement de la quête réussie`)}
 
 
        }
--- commands/admin/test.js
+++ commands/admin/test.js
@@ -1,6 +1,6 @@
 const superagent = require('superagent').agent();
        const dotenv = require('dotenv'); dotenv.config();
-       const {MessageEmbed, Formatters} = require('discord.js')
+       const {MessageEmbed, Formatters, MessageAttachment} = require('discord.js')
        const dayjs = require ('dayjs');
        
 module.exports = {
@@ -12,28 +12,7 @@ module.exports = {
     examples: ['test'],
     description: 'Commande test',
        async run (client, message, args) {
-                    const Mtest = await message.channel.send(`${message.author.username}, souhaitez-vous réellement lancer la commande test ? (o/n)`)              
-                    var jour = `${dayjs().format("ddd")}`; var j = 0
-                     while (jour != "Tue") { var j = j +1
-                            var jour = `${dayjs().add(+j, 'day').format("ddd")}`;
-                     };
-                     var date = `${dayjs().add(+j, 'day').format("YYYY-MM-DD")}`
-                     console.log(date)
-                     const yo = Formatters.time(dayjs(`${date}T14:00`).unix(), Formatters.TimestampStyles.RelativeTime)
-                    
-                     console.log(Mtest.id, Mtest.createdTimestamp);
-                     message.channel.send(`${yo}`)
-                      
-                     var accepting = true
-                     await new Promise(resolve => {
-                            setTimeout(() => {resolve(); if (accepting) {message.channel.send("Le temps est écoulé."); return accepting = false}}, 5000)
-                     client.on("messageCreate", message => {
-                            if (!accepting) return
-                            if (message.author.bot) return
-                            if (message.content == "o" || message.content == "O") { message.channel.send('Execution de la commande');resolve(); return accepting = false}
-                            if (message.content == "n" || message.content == "N") { message.channel.send('Annulation de la commande');resolve(); return accepting = false}
-                     })}); var accepting = false
-                     await new Promise(resolve => setTimeout(resolve, 1000))
-                     return message.channel.send("Fin de l'éxécution de la commande test")
+              var image = new MessageAttachment('https://cdn.wolvesville.com/promos/mysticangel.jpg')
+              message.channel.send({files: [image]})
        }
 }
